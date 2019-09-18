d8760a245e26f6107431b02fc5a02821898aaa5e
T-Ying Lin <tyinglin08@ksu.edu>
2019-09-18 18:39:58 -0500

    Added Kelvin to temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -4,5 +4,7 @@ echo -n "Enter temperature (F) : "
 read tf
 # formula Tc=(5/9)*(Tf-32)
 tc=$(echo "scale=4;(5/9)*($tf-32)"|bc)
+tk=$(echo "scale=4;$tc+273.15"|bc)
 echo "Temperature (C) = $tc"
+echo "Temperature (K) = $tk"
 exit 1
